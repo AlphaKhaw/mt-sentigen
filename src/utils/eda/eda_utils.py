7cd9bc8ca38bd20abd24cbf9c44d8962352c31af
Jia Hao Khaw <jia_hao_khaw@aiap.sg>
2023-08-06 18:40:52 +0800

    Add utils functions for inital exploration of dataset notebook

--- src/utils/eda/eda_utils.py
+++ src/utils/eda/eda_utils.py
@@ -4,6 +4,7 @@ import logging
 import os
 from typing import Any, Dict, Iterator
 
+import pandas as pd
 import requests
 
 logging.warnings.filterwarnings("ignore")
@@ -59,9 +60,59 @@ def parse(path: str) -> Iterator[Dict[str, Any]]:
         gzip_file = gzip.open(path, "r")
         for line in gzip_file:
             yield json.loads(line)
-    except json.JSONDecodeError:
-        logging.info(f"JSONDecodeError occurred for file: {path}")
-        return
     except Exception as e:
         logging.info(f"An error occurred while parsing file {path}: {e}")
         return
+
+
+def parse_into_dataframe(path: str) -> pd.DataFrame:
+    """
+    Parse a Gzip-compressed JSON file or read a Gzip-compressed CSV file and
+    convert it into a pandas DataFrame.
+
+    Args:
+        path (str): The file path to the Gzip-compressed JSON or CSV file.
+
+    Returns:
+        pd.DataFrame: A pandas DataFrame containing the data parsed from the
+            Gzip-compressed JSON file, or the data read from the
+            Gzip-compressed CSV file.
+    """
+    if path.endswith(".json.gz"):
+        return pd.DataFrame(parse(path))
+    elif path.endswith(".csv.gz"):
+        return pd.read_csv(path, compression="gzip")
+    else:
+        raise ValueError(
+            "Unsupported file format. Only Gzip-compressed JSON (.json.gz) or \
+                CSV (.csv.gz) files are supported."
+        )
+
+
+def remove_files_from_folder(folder_path: str) -> None:
+    """
+    Remove files from the specified folder.
+
+    Args:
+        folder_path (str): The path to the folder from which files will be
+            removed.
+
+    Raises:
+        FileNotFoundError: If the provided folder_path does not exist.
+        PermissionError: If the user does not have permission to remove files
+            from the folder.
+
+    Returns:
+        None
+    """
+    files = os.listdir(folder_path)
+    for file in files:
+        file_path = os.path.join(folder_path, file)
+        try:
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+                logging.info(f"File '{file}' has been removed.")
+            else:
+                logging.info(f"'{file}' is not a file. Skipping...")
+        except Exception as e:
+            logging.info(f"An error occurred while removing '{file}': {e}")
