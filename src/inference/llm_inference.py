227dc3ab4b2ce3c0709435742d5da13224a8313d
AlphaKhaw <87654386+AlphaKhaw@users.noreply.github.com>
2023-09-16 11:38:08 +0800

    #23 - LLM (#26)
    * #23: Create notebook to experiment with GPT4All LLMs
    
    * #23: Clean up notebook
    
    * #23: Add inference scripts
    
    * #23: Add LLM parameters to Hydra config file
    
    * #23: Add LLM experiment notebook
    
    * #23: Add LLM FastAPI script
    
    * #23: Modify inference and FastAPI script
    
    * #23: Add Dockerfiles for CPU, GPU and Deployment of FastAPI
    
    * #23: Add requirements for deployment of LLM
    
    * #23: Add inference scripts for llama-cpp and gpt4all
    
    * #23: Add docker-compose yaml
    
    * #23: Add yaml configurations for LLM FastAPI script
    
    * #23: Update conda env file with pip dependencies
    
    * #23: Add notebook for experimentation
    
    * #23: Add simple Python script to test usability of FastAPI
    
    ---------
    
    Co-authored-by: Jia Hao Khaw <jia_hao_khaw@aiap.sg>

--- src/inference/llm_inference.py
+++ src/inference/llm_inference.py
@@ -1,57 +1,70 @@
 import logging
-from typing import Dict, List, Union
+from typing import Dict, List
 
-import pandas as pd
-from gpt4all import GPT4All
+from colorama import Fore, Style, init
+from llama_cpp import Llama
 
+init(autoreset=True)
 
-class DummyLLMModel:
-    def generate(self, prompt: str, max_tokens: int) -> str:
-        return '{"sentiment": "Positive", "response": "Thank you for your kind words!"}'
-
-
-# Initialize your LLM models here.
-LLM_MODELS = {
-    "LLM1": {
-        "model": DummyLLMModel(),
-        "prompt_template": "Your LLM1 Prompt Template here...",
-    },
-    "LLM2": {
-        "model": DummyLLMModel(),
-        "prompt_template": "Your LLM2 Prompt Template here...",
-    }
-    # Add more models if needed
-}
+logging.warnings.filterwarnings("ignore")
+logging.basicConfig(level=logging.INFO)
 
 
-def generate_prompt(review: str, template: str) -> str:
-    return template.format(review=review)
+def custom_log(msg: str, color: str) -> None:
+    """
+    Log a custom message with a specified color.
 
+    Args:
+        msg (str): The message to be logged.
+        color (str): The color code to be used for the message.
 
-def post_process_output(output: str) -> Dict[str, Union[str, str]]:
-    processed_output = eval(output)
-    return {
-        "sentiment": processed_output.get("sentiment", "Unknown"),
-        "response": processed_output.get("response", "Unknown"),
-    }
+    Returns:
+        None
+    """
+    logging.info(color + msg + Style.RESET_ALL)
 
 
 def predict(
-    model_name: str, input_reviews: List[str]
-) -> List[Dict[str, Union[str, str]]]:
-    model_info = LLM_MODELS.get(model_name)
-    if model_info is None:
-        return [{"error": f"Model {model_name} not found"}]
+    model: Llama,
+    input_reviews: List[str],
+    config: dict,
+) -> Dict[str, dict]:
+    """
+    Generate predictions for a list of reviews using a specified model and
+    configuration settings.
 
-    model = model_info["model"]
-    prompt_template = model_info["prompt_template"]
+    Parameters:
+        model (Llama): The Llama model object to be used for generating
+            predictions.
+        input_reviews (List[str]): A list of review strings for which
+            predictions will be generated.
+        config (dict): A dictionary containing the configuration settings,
+            including 'generate_parameters' and 'prompts'.
 
-    results = []
+    Returns:
+        Dict[str, dict]: A dictionary where keys are the review strings and the
+            values are the generated predictions.
 
+    Note:
+        The function attempts to generate a prediction multiple times
+        (up to 'max_attempts') for each review if it fails to evaluate the
+        model output.
+    """
+    max_attempts = config["generate_parameters"]["max_attempts"]
+    results = {}
     for review in input_reviews:
-        prompt = generate_prompt(review, prompt_template)
-        output = model.generate(prompt, max_tokens=150).strip()
-        processed_output = post_process_output(output)
-        results.append(processed_output)
+        prompt = config["prompts"]["Llama-2-7B Chat"]["instruction"].replace(
+            "{review}", review
+        )
+        for _ in range(max_attempts):
+            output = model(
+                prompt, max_tokens=config["generate_parameters"]["max_tokens"]
+            )
+            try:
+                evaluated_output = eval(output["choices"][0]["text"])
+                results[review] = evaluated_output
+                break
+            except Exception as e:
+                custom_log(f"{e} - Re-attempting LLM Generation", Fore.RED)
 
     return results
